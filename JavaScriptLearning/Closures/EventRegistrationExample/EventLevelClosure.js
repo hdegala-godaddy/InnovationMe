1871b0a0c0c90db8296af904e82abcc99c8a6628
harsha d <chaitanya.harsha@gmail.com>
2017-02-05 20:20:26 +0000

    Event Listner Example

--- JavaScriptLearning/Closures/EventRegistrationExample/EventLevelClosure.js
+++ JavaScriptLearning/Closures/EventRegistrationExample/EventLevelClosure.js
@@ -1,6 +1,9 @@
 
+//Following code registers onclick handlers, to alert the level of the element in the DOM
 
-//Bad way of doing it
+
+//Bad Way : which does not work way of doing it,
+//The alert always shows 11 where ever or which ever element you click
 function assignEventsForElements(elems){
 
 
@@ -13,10 +16,19 @@ function assignEventsForElements(elems){
 }
 
 
+//Good Way : Un Comment the following to understand real ussage of closure
+//Here instead of directly assigning a function to onclick, we create a closure and assign it
+// function assignEventsForElements(elems){
 
 
+// 	for (var i=0;i<elems.length;i++){
 
-
+// 		elems[i].onclick = function(i){
+// 		return function(){
+// 			alert(i);
+// 		}}(i);
+// 	}
+// }
 
 assignEventsForElements(document.getElementsByTagName("*"));
 
